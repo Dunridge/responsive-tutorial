366775a85f9641da55ee5cabdb625e8fd39f1eca
Dunridge <Polinka7max@gmail.com>
2021-01-23 12:09:40 +0200

    added the header and the menu

--- tours/scripts.js
+++ tours/scripts.js
@@ -1,1 +1,12 @@
-//start
+// function expression to select elements
+const selectElements = (s) => document.querySelector(s);
+
+//open the menu on click 
+selectElements('.open').addEventListener('click', () => {
+    selectElements('.nav-list').classList.add('active');
+});
+
+//close the menu on click 
+selectElements('.close').addEventListener('click', () => {
+    selectElements('.nav-list').classList.remove('active');
+});
